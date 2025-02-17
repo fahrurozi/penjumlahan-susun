71a6d4e2c26c4e62c07f024df4b1a5c08a0a57b5
Muhammad Fahru Rozi <m.fahruu.rozii@gmail.com>
2025-02-17 22:17:31 +0700

    fixing

--- myapp/views.py
+++ myapp/views.py
@@ -1,26 +1,15 @@
-from django.shortcuts import render
-from django.conf import settings
-from django.http import JsonResponse
 import os
 import json
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.core import serializers
-from django.core.serializers import serialize
-
-# Create your views here.
+from django.shortcuts import render
+from django.conf import settings
 
 def home(request):
-    file_path = staticfiles_storage.path('data/data.json')
-    with open(file_path, 'r') as file:
-        file_content = file.read()
-        
-    # Parse the JSON content to ensure it's valid JSON
-        
-    json_data = json.loads(file_content)
+    file_path = os.path.join(settings.BASE_DIR, 'myapp', 'static', 'data', 'data.json')
 
-# Serialize the JSON data to a JSON string
-    companies_json = json.dumps(json_data, ensure_ascii=False)
-        
-        
-    return render(request, 'home.html', {'data': json.dumps(json_data)})
-        # return JsonResponse(json_data, safe=False)
+    if not os.path.exists(file_path):
+        return render(request, 'home.html', {'error': 'File data.json tidak ditemukan'})
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    return render(request, 'home.html', {'data': json.dumps(json_data, ensure_ascii=False)})
